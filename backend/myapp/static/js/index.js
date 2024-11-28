e71033798da372a6add98caa96ac32aa10d9cafe
Anna <annacarolinataboada@gmail.com>
2024-11-28 16:50:36 +0000

    pong classes in one file backup

--- backend/myapp/static/js/index.js
+++ backend/myapp/static/js/index.js
@@ -106,20 +106,41 @@ function buildChatFriendsList() {
 
 				var newElement = document.createElement('li');
 				newElement.className = 'chatFriendListContent p-2 border-bottom';
+				// newElement.innerHTML = `
+				// 		<a class="chatFriendListContent_a d-flex justify-content-between" style="cursor: pointer;">
+				// 			<div class="d-flex flex-row">
+				// 				<img src="${item.profile_pic}" width="50" height="50" class="d-flex align-self-center me-3 rounded-circle" onerror="this.onerror=null; this.src='/media/default.jpg';">
+				// 				<div class="pt-1">
+				// 					<p class="fw-bold mb-0">${item.username}</p>
+				// 					<p id="lastMsg_${roomName}" class="smallMessage">-</p>
+				// 				</div>
+				// 			</div>
+				// 			<div class="pt-1 d-flex flex-column">
+				// 				<div class="p-1">
+				// 					<p id="timeLastMsg_${roomName}" class="smallTime mb-1">-</p>
+				// 				</div>
+				// 				<div class="p-1 d-flex justify-content-end">
+				// 					<span class="online-indicator"></span>
+				// 				</div>
+				// 			</div>
+				// 		</a>
+				// 	</li>`;
 				newElement.innerHTML = `
 						<a class="chatFriendListContent_a d-flex justify-content-between" style="cursor: pointer;">
-							<div class="d-flex flex-row">
-								<img src="${item.profile_pic}" width="50" height="50" class="d-flex align-self-center me-3 rounded-circle" onerror="this.onerror=null; this.src='/media/default.jpg';">
-								<div class="pt-1">
-									<p class="fw-bold mb-0">${item.username}</p>
-									<p id="lastMsg_${roomName}" class="smallMessage"></p>
-								</div>
+						<div class="d-flex flex-row position-relative">
+							<div class="position-relative">
+								<img src="${item.profile_pic}" width="50" height="50" class="rounded-circle" onerror="this.onerror=null; this.src='/media/default.jpg';">
+								<span class="online-indicator"></span>
 							</div>
-							<div class="pt-1">
-								<p id="timeLastMsg_${roomName}" class="smallTime mb-1">-</p>
+							<div class="pt-1 ms-3">
+								<p class="fw-bold mb-0">${item.username}</p>
+								<p id="lastMsg_${roomName}" class="smallMessage">-</p>
 							</div>
-						</a>
-					</li>`;
+						</div>
+						<div class="pt-1">
+							<p id="timeLastMsg_${roomName}" class="smallTime mb-1">-</p>
+						</div>
+					</a>`;
 				newElement.onclick = (function() {
 					openChat(roomName, newElement);
 					document.querySelector('.blockButtonClass').onclick = function() {
